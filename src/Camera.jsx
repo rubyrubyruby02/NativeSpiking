a93dc6f0133f42f5e16e0aa6e03a49c8ac85421a
annabenari <annabenari@me.com>
2023-09-15 12:06:32 +0100

    attempting to see preview or image

--- src/Camera.jsx
+++ src/Camera.jsx
@@ -1,15 +1,22 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Camera, CameraType } from "expo-camera";
-import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+} from "react-native";
 import styles from "./styling";
 
 export default function TestCamera() {
   const [type, setType] = useState(CameraType.back);
   const [hasPermission, setHasPermission] = useState(null);
   const [capturedImage, setCapturedImage] = useState(null);
-  const [preview, setPreview] = useState(false);
+  // const [preview, setPreview] = useState(false);
 
-  const cameraRef = useRef();
+  const cameraRef = useRef(null);
 
   useEffect(() => {
     Camera.requestCameraPermissionsAsync()
@@ -38,30 +45,46 @@ export default function TestCamera() {
       const data = await cameraRef.current.takePictureAsync();
       const source = data.uri;
       if (source) {
-        setPreview(true);
+        console.log(source);
         setCapturedImage(source);
       }
     }
   };
 
-  if (preview && capturedImage) {
-    
-  }
-  
-
   return (
     <View style={styles.cameraContainer}>
-      <Camera style={styles.camera} type={type} ref={cameraRef}>
-        <View style={styles.buttonContainer}>
-            <Button style={styles.button} onPress={toggleCameraType} title="Flip camera" color="white">
-            </Button>
+      {!capturedImage ? (
+        <Camera style={styles.camera} type={type} ref={cameraRef}>
+          <View style={styles.buttonContainer}>
+            <Button
+              style={styles.button}
+              onPress={toggleCameraType}
+              title="Flip camera"
+              color="white"
+            ></Button>
+          </View>
+          <View>
+            <Button
+              style={styles.button}
+              onPress={takePicture}
+              title="Take Pic"
+              color="white"
+            ></Button>
+          </View>
+        </Camera>
+      ) : (
+        <View style={styles.previewImage}>
+          <Image
+            source={{ source: capturedImage }}
+            style={{ width: 300, height: 400 }}
+          ></Image>
+          <Text>hELLO</Text>
+          <Image
+            source={{ source: capturedImage }}
+            style={{ width: 300, height: 400 }}
+          ></Image>
         </View>
-        <View>
-            <Button style={styles.button} onPress={takePicture} title="Take Pic" color="white">
-            </Button>
-        </View>
-
-      </Camera>
+      )}
     </View>
   );
 }
